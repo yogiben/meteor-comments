31d15fab498c769ba568434c83d072286e8ac6b8
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2014-09-26 12:19:36 +0200

    Versioning

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Adds comments to your collection(s) of choice",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "http://github.com/yogiben/meteor-comments.git"
 });
 
